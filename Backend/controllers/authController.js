57821303c5ff034fcda4108f2068f89e3227f4ac
Ramyaa Balasubramanian <145574776+ramyaa-b@users.noreply.github.com>
2025-06-24 16:43:11 +0530

    initial backend
    mongoDB connected

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,7 @@
+exports.loginUser = (req, res) => {
+    res.json({ message: "Login endpoint hit" });
+};
+
+exports.registerUser = (req, res) => {
+    res.json({ message: "Register endpoint hit" });
+};
---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db.js");
+const authRoutes = require("./routes/authRoutes");
+
+
+connectDB(); // Import and connect to MongoDB
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+
